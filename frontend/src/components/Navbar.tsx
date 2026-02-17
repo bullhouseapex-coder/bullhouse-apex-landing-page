7e1fd880cc6eb838f010cf407b141cf42cbc9400
Marco Luis Hernandez <marcoluis.hernandez@gmail.com>
2026-02-17 13:05:30 +0800

    Landing Page Navbar - Clearing unused code

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,54 +1,5 @@
-import { Button } from "@/components/ui/button";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
-const placeholders: { title: string; href: string; description: string }[] = [
-  {
-    title: "Title 1",
-    href: "/title1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure itaque ad magni? Totam aliquam sed quasi in saepe repellat, animi blanditiis, magnam unde accusamus, repellendus vitae! Vero atque, rem praesentium impedit corporis repudiandae distinctio blanditiis totam accusantium explicabo veniam numquam!",
-  },
-  {
-    title: "Title 2",
-    href: "/title2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure itaque ad magni? Totam aliquam sed quasi in saepe repellat, animi blanditiis, magnam unde accusamus, repellendus vitae! Vero atque, rem praesentium impedit corporis repudiandae distinctio blanditiis totam accusantium explicabo veniam numquam!",
-  },
-  {
-    title: "Title 3",
-    href: "/title3",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure itaque ad magni? Totam aliquam sed quasi in saepe repellat, animi blanditiis, magnam unde accusamus, repellendus vitae! Vero atque, rem praesentium impedit corporis repudiandae distinctio blanditiis totam accusantium explicabo veniam numquam!",
-  },
-  {
-    title: "Title 4",
-    href: "/title4",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure itaque ad magni? Totam aliquam sed quasi in saepe repellat, animi blanditiis, magnam unde accusamus, repellendus vitae! Vero atque, rem praesentium impedit corporis repudiandae distinctio blanditiis totam accusantium explicabo veniam numquam!",
-  },
-  {
-    title: "Title 5",
-    href: "/title5",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure itaque ad magni? Totam aliquam sed quasi in saepe repellat, animi blanditiis, magnam unde accusamus, repellendus vitae! Vero atque, rem praesentium impedit corporis repudiandae distinctio blanditiis totam accusantium explicabo veniam numquam!",
-  },
-  {
-    title: "Title 6",
-    href: "/title6",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure itaque ad magni? Totam aliquam sed quasi in saepe repellat, animi blanditiis, magnam unde accusamus, repellendus vitae! Vero atque, rem praesentium impedit corporis repudiandae distinctio blanditiis totam accusantium explicabo veniam numquam!",
-  },
-];
 
 export default function Navbar() {
   const [scrolled, setScrolled] = useState(false);
@@ -104,20 +55,5 @@ export default function Navbar() {
         </div>
       </div>
     </nav>
-  );
-}
-
-function ListItem({ title, children, href, ...props }: React.ComponentPropsWithoutRef<"li"> & { href: string }) {
-  return (
-    <li {...props}>
-      <NavigationMenuLink asChild>
-        <a href={href}>
-          <div className="flex flex-col gap-1 text-sm">
-            <div className="leading-none font-medium">{title}</div>
-            <div className="text-muted-foreground line-clamp-2">{children}</div>
-          </div>
-        </a>
-      </NavigationMenuLink>
-    </li>
   );
 }
